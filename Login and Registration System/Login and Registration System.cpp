7daeae273f4dec9cfcfcb53600c2c667fd0f907a
oliwilliams1 <imhungryoliver@gmail.com>
2024-05-26 17:14:33 +1200

    good mornign!

--- Login and Registration System/Login and Registration System.cpp
+++ Login and Registration System/Login and Registration System.cpp
@@ -1,9 +1,9 @@
-// Login and Registration System.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <nlohmann/json.hpp>
 #include <fstream>
+#include <vector>
+#include <cctype>
+
 using json = nlohmann::json;
 using namespace std;
 
@@ -15,26 +15,36 @@ json loadFile()
 
 json fileData = loadFile();
 
-bool isInputValid(string isInputValid, string type)
+string stringUpper(string string)
 {
-    if (isInputValid.length() <= 3) {
-        cout << type << " too short, must be atleast 5 charecters!\n";
-        return false;
-    }
-    else {
-        return true;
-    }
+    string[0] = toupper(string[0]);
+    return string;
 }
 
-int updateFile()
+bool isInputValid(string input, string type)
+{
+    if (input.length() < 5) {
+        cout << "\n" << type << " too short, must be atleast 5 charecters!\n";
+        return false;
+    }
+    if (type == "Username") {
+        if (fileData["Users"].count(input) != 0)
+        {
+            cout << "\nUsername already taken, please try again.\n";
+            return false;
+        }
+    }
+    return true;
+}
+
+void updateFile()
 {
     string jsonString = fileData.dump(2);
     ofstream jsonFile("registration.json");
     jsonFile << jsonString;
-    return 0;
 }
 
-int signUp()
+void signUp()
 {
     bool usernameValid = false;
     bool passwordValid = false;
@@ -53,11 +63,71 @@ int signUp()
         passwordValid = isInputValid(password, "Password");
     }
     fileData["Users"][username]["password"] = password;
+    fileData["Users"][username]["bio"] = "Empty!";
     updateFile();
-    return 0;
 }
 
-int logIn()
+void changeUserOrPass(string option, json* userData, string user)
+{
+    cout << "Enter your new " << option << "\n";
+    string output;
+    bool inputValid = false;
+    while (!inputValid)
+    {
+        getline(cin, output);;
+        inputValid = isInputValid(output, stringUpper(option));
+    }
+    if (option == "username")
+    {
+        fileData["Users"][output] = *userData;
+        fileData["Users"].erase(user);
+    }
+    else {
+        fileData["Users"][user][option] = output;
+    }
+    
+    updateFile(); // update the file
+}
+
+
+void profileScreen(string user, json* userPtr)
+{
+    bool optionValid = false;
+    json userData = *userPtr;
+
+    while (!optionValid)
+    {
+        cout << "\nWelcome, " << user << "!\n";
+        cout << "biography: " << userData["biography"] << "\n\n";
+        cout << "Options:\n";
+        cout << "1: Change username\n";
+        cout << "2: Change password\n";
+        cout << ":3 Edit bio\n";
+        cout << "4: Log out\n";
+        string output;
+        cin >> output;
+
+        switch (output[0])
+        {
+        case '1':
+            optionValid = true;
+            changeUserOrPass("username", userPtr, user);
+
+        case '2':
+            optionValid = true;
+            changeUserOrPass("password", userPtr, user);
+
+        case '3':
+            optionValid = true;
+            changeUserOrPass("biography", userPtr, user);
+
+        case '4':
+            optionValid = true;
+        }
+    }
+}
+
+void logIn()
 {
     bool validUsername = false;
     bool validPassword = false;
@@ -83,7 +153,6 @@ int logIn()
 
         if (fileData["Users"][username]["password"] == password)
         {
-            cout << "Logged in!";
             validPassword = true;
             validUsername = false;
         }
@@ -91,10 +160,48 @@ int logIn()
             cout << "Invalid password, try again:\n";
         }
     }
-    return 0;
+    profileScreen(username, &fileData["Users"][username]);
+}
+
+void welcomeScreen()
+{
+    cout << "Welcome to bla h bl center\n";
+    cout << "1: Sign Up\n";
+    cout << "2: Log In\n";
+    cout << "Input option:\n";
+}
+
+void startingScreen()
+{
+    while (true)
+    {
+        welcomeScreen();
+        string option;
+        bool validOption = false;
+        while (!validOption)
+        {
+            cin >> option;
+            switch (option[0]) {
+            case '1':
+                signUp();
+                validOption = true;
+                break;
+
+            case '2':
+                logIn();
+                validOption = true;
+                break;
+
+            default:
+                cout << "Invalid option!\n\n";
+                welcomeScreen();
+                break;
+            }
+        }
+    }
 }
 
 int main()
 {
-    logIn();
+    startingScreen();
 }
